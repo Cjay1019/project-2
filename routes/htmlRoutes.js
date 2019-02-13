1a381b84b1e3e6c65ed43225e07c90a4995c98c7
IamMonkie <monkie_225@yahoo.com>
2019-02-12 21:37:33 -0500

    login page served

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,19 @@
 var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Load example page and pass in an example by id
