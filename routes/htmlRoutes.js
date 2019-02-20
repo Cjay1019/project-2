fa742238e3af1e12806d4832e6589d13c1cff9d3
ajmesa <amesa987@gmail.com>
2019-02-19 21:29:02 -0500

    Logic Complete

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,11 @@
 
 module.exports = function(app) {
   // Load index page
-<<<<<<< HEAD
   app.get("/", function(req, res) {
     res.render("index");
   });
-=======
->>>>>>> 57f9cc11e53bf0943680a55298cfc13c3ebdf846
 
   // Load example page and pass in an example by id
-  app.get("/", function(req, res) {
-    res.render("index");
-  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
