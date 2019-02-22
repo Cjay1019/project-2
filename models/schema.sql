9f696d56f6365cf0a0b335884f83d90e70599bd2
Cjay1019 <cjay741@gmail.com>
2019-02-21 20:16:28 -0500

    player 1 removes gif on start

--- models/schema.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS trivia_db;
-
-CREATE DATABASE trivia_db;
-
-USE trivia_db;
--- models/users.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var users = sequelize.define("users", {
-    user_name: DataTypes.STRING,
-    wins: DataTypes.INTEGER
-  });
-  return users;
-};
